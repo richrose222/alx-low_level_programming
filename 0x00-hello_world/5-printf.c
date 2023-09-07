a43165100d13f34c545cc6597345393949a1a2c3
richrose222 <richrose222@gmail.com>
2023-09-07 12:45:01 -0700

    hello prinf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this programme prints exactly what you want
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
