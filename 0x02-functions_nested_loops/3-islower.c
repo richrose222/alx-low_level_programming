cac0d06f36afdda7952fd7434927ac08030d1446
richrose222 <richrose222@gmail.com>
2023-09-13 07:44:59 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - main function
+ *@c: the character to be checked
+ * Return: void
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	
+	return (1);
+	}
+	return (0);
+}
