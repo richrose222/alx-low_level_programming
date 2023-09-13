0b105f4aed04914d0b67e77506167445b7890c5f
richrose222 <richrose222@gmail.com>
2023-09-13 09:23:16 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute
+ * @c: the numberto comp
+ * Return: Absolute value of number or zero
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
